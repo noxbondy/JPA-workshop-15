f01ae36193d7157bbfd9f599b8e649dcc5adfe34
md gulam noxbondy <gulamnoxbondy@gmail.com>
2025-04-12 19:58:30 +0200

    all test done

--- src/main/java/se/lexicon/jpaworkshop/repository/AppUserRepository.java
+++ src/main/java/se/lexicon/jpaworkshop/repository/AppUserRepository.java
@@ -5,17 +5,17 @@ import org.springframework.stereotype.Repository;
 import se.lexicon.jpaworkshop.entity.AppUser;
 
 import java.time.LocalDate;
+
 import java.util.List;
-import java.util.Optional;
+;
 
 @Repository
-public interface AppUserRepository extends CrudRepository<AppUser, Long> {
+public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
 
-    Optional<AppUser> findByUsername(String username);
-    List<AppUser> findByRegistrationDateBetween(LocalDate startDate, LocalDate endDate);
-    List<AppUser> findByDetailsId(int detailsId);
-    Optional<AppUser> findByEmailIgnoreCase(String email);
-
+    AppUser findByUsername(String username);
+    List<AppUser> findByRegDateBetween(LocalDate regDateAfter, LocalDate regDateBefore);
+    List<AppUser>findByDetails_Id(Integer id);
+    List<AppUser>findByUsernameContainingIgnoreCase(String username);
 
 
 }
